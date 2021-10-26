a1937bc8015be6446b07b3a31874f85a027d2aff
Luke Cho <luke.h.cho@gmail.com>
2021-10-26 15:25:17 -0400

    test

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import Header from './components/Header.jsx';
+
+const App = () => {
+
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
+
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+
+const Header = (props) => {
+  return (
+    <div>
+      <div>
+        <a href="/"><img src='/'/>Home</a>
+      </div>
+      <div>
+        <a href="https://github.com/oslabs-beta/kubric"><img src='/'/></a>
+      </div>
+      <div>
+
+      </div>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Header;
