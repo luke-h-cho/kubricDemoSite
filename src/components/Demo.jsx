829a61d79f8b09023b078b309b8d68f8173c550f
jlhline <jlhline@gmail.com>
2021-10-27 13:16:27 -0400

    demo styles

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,9 +1,25 @@
 import React, {useState} from 'react';
-import gif1 from '../assets/KubricLogDemoTest.gif'
+import kubricLogDemoTest from '../assets/KubricLogDemoTest.gif'
+import kubricDemoNodeToggle from '../assets/KubricDemoNodeToggle.gif'
+import kubricTabToggleDemo from '../assets/KubricTabToggleDemo.gif'
 const Demo = (props) => {
   return (
     <div id="demo">
-      <img src={gif1}/>
+     <div className="logDemo">
+       <img className="imgStyle" width='360' height='240' src={kubricLogDemoTest}/>
+       <p className="demoText">Query Persistent Log Storage by Index Name, Field and Value</p>
+        </div>
+      <br/>
+      <div className="toggleDemo">
+      <p className="demoText">Toggle Visualization to view relative performance</p>
+      <img className="imgStyle" width='360' height='240' src={kubricDemoNodeToggle}/>
+      </div>
+      <br/>
+      <div className="tabDemo">
+      <img className="imgStyle" width='360' height='240' src={kubricTabToggleDemo}/>
+      <p className="demoText">Tab between Overview, Master and Worker Node views</p>
+      </div>
+      <br/>
     </div>
   );
 };
