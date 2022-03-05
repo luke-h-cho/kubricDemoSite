356e37f8b362266e94bebc75030f913e218f7936
jlhline <jlhline@gmail.com>
2022-03-05 01:43:02 -0500

    site update

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -4,32 +4,48 @@ import kubricDemoNodeToggle from '../assets/KubricDemoNodeToggle.gif'
 import kubricTabToggleDemo from '../assets/KubricTabToggleDemo.gif'
 import bg from '../assets/y-so-serious-white.png'
 import darkbg from '../assets/y-so-serious-dark.png'
+import Carousel from 'react-bootstrap/Carousel'
+
 const Demo = (props) => {
+  const styleOverride = {
+    firstSlide:{
+      color:'black'
+    },
+    otherSlides:{
+      fontSize:'1.5vw',
+      fontFamily: 'Arial, Helvetica, sans-serif',
+      fontWeight:'bold'
+    }
+
+   
+  }
   return (
-    <div id="demo" style={{
-      backgroundImage: `url(${bg})`
-    }}>
-     <div className="logDemo">
-       <img className="imgStyle" src={kubricLogDemoTest}/>
-       <p style={{
-      backgroundImage: `linear-gradient(to top,rgba(50, 116, 109,.7),rgba(206, 224, 220,.4)),url(${bg})`
-    }}className="demoText">Query Persistent Log Storage by Index Name, Field and Value</p>
-        </div>
-      <br/>
-      <div className="toggleDemo">
-      <p style={{
-      backgroundImage: `linear-gradient(to top,rgba(50, 116, 109,.7),rgba(206, 224, 220,.4)),url(${bg})`
-    }}className="demoText">Toggle Visualization to view relative performance</p>
-      <img className="imgStyle" src={kubricDemoNodeToggle}/>
-      </div>
-      <br/>
-      <div className="tabDemo">
-      <img className="imgStyle" src={kubricTabToggleDemo}/>
-      <p style={{
-      backgroundImage: `linear-gradient(to top,rgba(50, 116, 109,.7),rgba(206, 224, 220,.4)),url(${bg})`
-    }}className="demoText">Tab between Overview, Master and Worker Node views</p>
-      </div>
-      <br/>
+    <div id="demo" style={{backgroundImage: `url(${bg})`}} >
+      <div id="carouselHolder">
+      <Carousel>
+  <Carousel.Item >
+  <img className="imgStyle" src={kubricLogDemoTest}
+  />
+    <Carousel.Caption 
+>
+      <h3 style={{...styleOverride.otherSlides,...styleOverride.firstSlide,}}>Query Persistent Log Storage by Index Name, Field and Value.</h3>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item >
+  <img className="imgStyle" src={kubricDemoNodeToggle}/>
+    <Carousel.Caption>
+      <h3 style={styleOverride.otherSlides}>Toggle Visualization to view relative performance.</h3>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item >
+  <img className="imgStyle" src={kubricTabToggleDemo}/>
+
+    <Carousel.Caption>
+      <h3 style={styleOverride.otherSlides}>Tab between Overview, Master and Worker Node views.</h3>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+</div> 
     </div>
   );
 };
--- src/components/Filler.jsx
+++ src/components/Filler.jsx
@@ -1,18 +1,24 @@
-import React, {useState} from 'react';
+import React, {Text,useState} from 'react';
 import kubric from '../assets/kubric-colored-skeleton.png'
 import bg from '../assets/y-so-serious-dark.png'
-
+import lightbg from '../assets/y-so-serious-white.png'
+import LogSVG from './LogSVG.jsx'
+import GraphSVG from './GraphSVG.jsx'
 const Filler = (props) => {
   return (
-    <div id="filler" style={{
-      backgroundImage: `linear-gradient(to top,rgba(255, 255, 255 ,.02),rgba(255, 255, 255 ,.2)),url(${bg})`
-    }}>
-      <p>What is Kubric?<br/><br/>
-      Kubric is a web application that displays key cluster metrics 
-      of Kubernetes Node and Pod processes as well as remotely persisted 
-      application logs to query. 
-      </p>
+    <div id="filler">
       
+    <span id="headline"><em>Monitor</em> your <em>Kubernetes</em> cluster</span>
+    
+      <div style={{'border-bottom':'4px solid rgb(34, 40, 38)'}}>
+      <LogSVG/>
+     <p>Kubric is a web application that queries remotely persisted application logs and displays important cluster metrics. </p> 
+      <GraphSVG/>
+      
+      </div>
+      <span style={{
+      backgroundImage: `url(${bg})`
+    }}></span>
     </div>
   )
 }
